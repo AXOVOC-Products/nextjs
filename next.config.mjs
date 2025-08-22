aaf8e8b35b28778b2fea3804e31225a21561fd8c
psy10 <s7pan@uwaterloo.ca>
2025-08-22 15:44:29 -0400

    domain fix trial #1

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  basePath: isProd ? '/nextjs' : '',
-  assetPrefix: isProd ? '/nextjs/' : '',
   output: 'export',
   distDir: 'dist',
   eslint: {
